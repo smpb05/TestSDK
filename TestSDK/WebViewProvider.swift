7279be4b31cb152c55ab2ae305263bca412810a1
mika-no-olala <107396363+mika-no-olala@users.noreply.github.com>
2024-05-14 15:28:12 +0500

    readme

--- TestSDK/WebViewProvider.swift
+++ TestSDK/WebViewProvider.swift
@@ -48,6 +48,7 @@ public class WebViewProvider {
     
     public func setUser(phone: String) {
         let js = "setUserData('\(phone)');"
+//        let js = "window.setUserData({phone: \"+7777777\"});"
         webView.evaluateJavaScript(js) { (result, error) in
             if let error = error {
                 print("Error: \(error)")
