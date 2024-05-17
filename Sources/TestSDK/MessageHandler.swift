f7b3dad0b97c7cbbc89f091a1813d1e0164dba23
mika-no-olala <107396363+mika-no-olala@users.noreply.github.com>
2024-05-17 15:12:02 +0500

    fix

--- Sources/TestSDK/MessageHandler.swift
+++ Sources/TestSDK/MessageHandler.swift
@@ -1,14 +1,8 @@
-//
-//  Tets.swift
-//  TestSDK
-//
-//  Created by Anastasia Filinskaya on 16.05.2024.
-//
 
 import Foundation
 import WebKit
 
-public class Tets: NSObject, WKScriptMessageHandler {
+public class MessageHandler: NSObject, WKScriptMessageHandler {
     
     public var callback: ((String) -> Void)?
     
@@ -22,7 +16,5 @@ public class Tets: NSObject, WKScriptMessageHandler {
                 print("MessageHandler: message body that I got from web page is not a String")
             }
         }
-        
     }
-    
 }
--- Sources/TestSDK/WebViewProvider.swift
+++ Sources/TestSDK/WebViewProvider.swift
@@ -5,10 +5,11 @@ import AVFoundation
 public class WebViewProvider {
 
     private var webView: WKWebView!
+    private var messageHandler: MessageHandler!
     private var permissionsAllowed: Bool = false
     private let url: URL = URL(string: "https://mvc.t2m.kz/demos/echotest.html")!
     
-    public init() {}
+    public init(){}
 
     public func loadPage() -> Bool {
         checkPermissions()
@@ -24,8 +25,10 @@ public class WebViewProvider {
             let preference = WKWebpagePreferences()
             preference.preferredContentMode = .mobile
             preference.allowsContentJavaScript = true
-            
             conf.defaultWebpagePreferences = preference
+        
+            webView.configuration.userContentController.add(messageHandler, name: "jsHandler")
+        
             webView.load(URLRequest(url: url))
             
             print("WebViewProvider: webView has loaded")
@@ -37,48 +40,26 @@ public class WebViewProvider {
             print("WebViewProvider: webView has loaded, but ios version is too low")
             return true
         }
-//
-//        print("WebViewProvider error: ios version problem")
-//        return false
     }
     
-    public func setWebView(webView: WKWebView) {
+    public func setWebView(webView: WKWebView, messageHandler: MessageHandler) {
         self.webView = webView
+        self.messageHandler = messageHandler
         requestPermissions()
     }
     
-    public func requestPermissions(completion: @escaping () -> Void) {
-        let group = DispatchGroup()
-        
-        group.enter()
-        AVCaptureDevice.requestAccess(for: .audio) {[weak self] granted in
-            guard self != nil else {return}
-            if(granted) {
-                DispatchQueue.main.async {
-//                    self.checkPermissions()
-                    group.leave()
-                }
+    public func setUser(phone: String) {
+        let js = "setUserData('\(phone)');"
+        webView.evaluateJavaScript(js) { (result, error) in
+            if let error = error {
+                print("WebViewProvider-setUser: \(error)")
+            } else {
+                print("WebViewProvider-setUser: Data have set successfully")
             }
-        }
-        
-        group.enter()
-        AVCaptureDevice.requestAccess(for: .video) {[weak self] granted in
-            guard self != nil else {return}
-            if(granted) {
-                DispatchQueue.main.async {
-//                    self.checkPermissions()
-                    group.leave()
-                }
-            }
-        }
-        
-        group.notify(queue: .main) {
-            self.checkPermissions()
-            completion()
         }
     }
     
-    public func requestPermissions() {
+    private func requestPermissions() {
         AVCaptureDevice.requestAccess(for: .video) {[weak self] granted in
             guard let self = self else {return}
             if(granted) {
@@ -98,7 +79,7 @@ public class WebViewProvider {
         }
     }
     
-    public func checkPermissions() {
+    private func checkPermissions() {
         let videoAuthorized = AVCaptureDevice.authorizationStatus(for: .video) == .authorized
         let audioAuthorized = AVCaptureDevice.authorizationStatus(for: .audio) == .authorized
         
